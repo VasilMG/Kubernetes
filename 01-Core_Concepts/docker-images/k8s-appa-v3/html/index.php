31da6ef77b93117d47d6b47a90d976c023bcbc6b
VasilMG <119819354+VasilMG@users.noreply.github.com>
2023-10-22 23:39:56 +0300

    Initial commit

---
+++ 01-Core_Concepts/docker-images/k8s-appa-v3/html/index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>World of Kubernetes</title>
+  </head>
+  <body>
+    <div align="center">
+      <img width="340px" height="340px" src="kubernetes-logo.png">
+      <h1>Welcome to the world of <span style="color: blue;">Kubernetes</span>!</h1>
+      <h2>There is plenty to explore here ...</h2>
+      <h2>You can start with the name: <span style="color: blue;">κυβερνήτης</span> &rArr; <span style="color: blue;">kubernetes</span> &rArr; <span style="color: blue;">k8s</span> :)</h2>
+      <h2>And then continue the journey with </h2><img src="minikube-logo.png">
+      <br />
+      <h2>And why stop here? Continue with other distributions like k0s, k3s, OpenShift, VMware Tanzu, etc.</h2>
+      <br /><br /><br />
+      Running on <b><?php echo gethostname(); ?></b>
+    </div>
+  </body>
+</html> 
